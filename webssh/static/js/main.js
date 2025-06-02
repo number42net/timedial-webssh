7cdcfe437d4ee0f197414109d068f785ca494671
number42net <github@number42.net>
2025-06-02 20:22:53 +0200

    Modify default colors

--- webssh/static/js/main.js
+++ webssh/static/js/main.js
@@ -4,16 +4,16 @@ var jQuery;
 var wssh = {};
 
 
-(function() {
+(function () {
   // For FormData without getter and setter
   var proto = FormData.prototype,
-      data = {};
+    data = {};
 
   if (!proto.get) {
     proto.get = function (name) {
       if (data[name] === undefined) {
         var input = document.querySelector('input[name="' + name + '"]'),
-            value;
+          value;
         if (input) {
           if (input.type === 'file') {
             value = input.files[0];
@@ -35,33 +35,33 @@ var wssh = {};
 }());
 
 
-jQuery(function($){
+jQuery(function ($) {
   var status = $('#status'),
-      button = $('.btn-primary'),
-      form_container = $('.form-container'),
-      waiter = $('#waiter'),
-      term_type = $('#term'),
-      style = {},
-      default_title = 'WebSSH',
-      title_element = document.querySelector('title'),
-      form_id = '#connect',
-      debug = document.querySelector(form_id).noValidate,
-      custom_font = document.fonts ? document.fonts.values().next().value : undefined,
-      default_fonts,
-      DISCONNECTED = 0,
-      CONNECTING = 1,
-      CONNECTED = 2,
-      state = DISCONNECTED,
-      messages = {1: 'This client is connecting ...', 2: 'This client is already connnected.'},
-      key_max_size = 16384,
-      fields = ['hostname', 'port', 'username'],
-      form_keys = fields.concat(['password', 'totp']),
-      opts_keys = ['bgcolor', 'title', 'encoding', 'command', 'term', 'fontsize', 'fontcolor', 'cursor'],
-      url_form_data = {},
-      url_opts_data = {},
-      validated_form_data,
-      event_origin,
-      hostname_tester = /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))|(^\s*((?=.{1,255}$)(?=.*[A-Za-z].*)[0-9A-Za-z](?:(?:[0-9A-Za-z]|\b-){0,61}[0-9A-Za-z])?(?:\.[0-9A-Za-z](?:(?:[0-9A-Za-z]|\b-){0,61}[0-9A-Za-z])?)*)\s*$)/;
+    button = $('.btn-primary'),
+    form_container = $('.form-container'),
+    waiter = $('#waiter'),
+    term_type = $('#term'),
+    style = {},
+    default_title = 'WebSSH',
+    title_element = document.querySelector('title'),
+    form_id = '#connect',
+    debug = document.querySelector(form_id).noValidate,
+    custom_font = document.fonts ? document.fonts.values().next().value : undefined,
+    default_fonts,
+    DISCONNECTED = 0,
+    CONNECTING = 1,
+    CONNECTED = 2,
+    state = DISCONNECTED,
+    messages = { 1: 'This client is connecting ...', 2: 'This client is already connnected.' },
+    key_max_size = 16384,
+    fields = ['hostname', 'port', 'username'],
+    form_keys = fields.concat(['password', 'totp']),
+    opts_keys = ['bgcolor', 'title', 'encoding', 'command', 'term', 'fontsize', 'fontcolor', 'cursor'],
+    url_form_data = {},
+    url_opts_data = {},
+    validated_form_data,
+    event_origin,
+    hostname_tester = /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))|(^\s*((?=.{1,255}$)(?=.*[A-Za-z].*)[0-9A-Za-z](?:(?:[0-9A-Za-z]|\b-){0,61}[0-9A-Za-z])?(?:\.[0-9A-Za-z](?:(?:[0-9A-Za-z]|\b-){0,61}[0-9A-Za-z])?)*)\s*$)/;
 
 
   function store_items(names, data) {
@@ -70,7 +70,7 @@ jQuery(function($){
     for (i = 0; i < names.length; i++) {
       name = names[i];
       value = data.get(name);
-      if (value){
+      if (value) {
         window.localStorage.setItem(name, value);
       }
     }
@@ -80,11 +80,11 @@ jQuery(function($){
   function restore_items(names) {
     var i, name, value;
 
-    for (i=0; i < names.length; i++) {
+    for (i = 0; i < names.length; i++) {
       name = names[i];
       value = window.localStorage.getItem(name);
       if (value) {
-        $('#'+name).val(value);
+        $('#' + name).val(value);
       }
     }
   }
@@ -92,11 +92,11 @@ jQuery(function($){
 
   function populate_form(data) {
     var names = form_keys.concat(['passphrase']),
-        i, name;
+      i, name;
 
-    for (i=0; i < names.length; i++) {
+    for (i = 0; i < names.length; i++) {
       name = names[i];
-      $('#'+name).val(data.get(name));
+      $('#' + name).val(data.get(name));
     }
   }
 
@@ -109,7 +109,7 @@ jQuery(function($){
   function decode_uri_component(uri) {
     try {
       return decodeURIComponent(uri);
-    } catch(e) {
+    } catch (e) {
       console.error(e);
     }
     return '';
@@ -120,7 +120,7 @@ jQuery(function($){
     try {
       return window.atob(encoded);
     } catch (e) {
-       console.error(e);
+      console.error(e);
     }
     return null;
   }
@@ -128,7 +128,7 @@ jQuery(function($){
 
   function parse_url_data(string, form_keys, opts_keys, form_map, opts_map) {
     var i, pair, key, val,
-        arr = string.split('&');
+      arr = string.split('&');
 
     for (i = 0; i < arr.length; i++) {
       pair = arr[i].split('=');
@@ -137,7 +137,7 @@ jQuery(function($){
 
       if (form_keys.indexOf(key) >= 0) {
         form_map[key] = val;
-      } else if (opts_keys.indexOf(key) >=0) {
+      } else if (opts_keys.indexOf(key) >= 0) {
         opts_map[key] = val;
       }
     }
@@ -180,7 +180,7 @@ jQuery(function($){
 
     var cols = parseInt(window.innerWidth / style.width, 10) - 1;
     var rows = parseInt(window.innerHeight / style.height, 10);
-    return {'cols': cols, 'rows': rows};
+    return { 'cols': cols, 'rows': rows };
   }
 
 
@@ -227,7 +227,7 @@ jQuery(function($){
     }
 
     if (custom_font_is_loaded()) {
-      var new_fonts =  custom_font.family + ', ' + default_fonts;
+      var new_fonts = custom_font.family + ', ' + default_fonts;
       term.setOption('fontFamily', new_fonts);
       term.font_family_updated = true;
       console.log('Using custom font family ' + new_fonts);
@@ -242,7 +242,7 @@ jQuery(function($){
     }
 
     if (default_fonts) {
-      term.setOption('fontFamily',  default_fonts);
+      term.setOption('fontFamily', default_fonts);
       term.font_family_updated = false;
       console.log('Using default font family ' + default_fonts);
     }
@@ -250,7 +250,7 @@ jQuery(function($){
 
 
   function format_geometry(cols, rows) {
-    return JSON.stringify({'cols': cols, 'rows': rows});
+    return JSON.stringify({ 'cols': cols, 'rows': rows });
   }
 
 
@@ -258,10 +258,10 @@ jQuery(function($){
     var reader = new window.FileReader();
 
     if (decoder === undefined) {
-      decoder = new window.TextDecoder('utf-8', {'fatal': true});
+      decoder = new window.TextDecoder('utf-8', { 'fatal': true });
     }
 
-    reader.onload = function() {
+    reader.onload = function () {
       var text;
       try {
         text = decoder.decode(reader.result);
@@ -289,7 +289,7 @@ jQuery(function($){
       encoding = 'utf-8';
     }
 
-    reader.onload = function() {
+    reader.onload = function () {
       if (callback) {
         callback(reader.result);
       }
@@ -359,20 +359,20 @@ jQuery(function($){
     }
 
     var ws_url = window.location.href.split(/\?|#/, 1)[0].replace('http', 'ws'),
-        join = (ws_url[ws_url.length-1] === '/' ? '' : '/'),
-        url = ws_url + join + 'ws?id=' + msg.id,
-        sock = new window.WebSocket(url),
-        encoding = 'utf-8',
-        decoder = window.TextDecoder ? new window.TextDecoder(encoding) : encoding,
-        terminal = document.getElementById('terminal'),
-        termOptions = {
-          cursorBlink: true,
-          theme: {
-            background: url_opts_data.bgcolor || 'black',
-            foreground: url_opts_data.fontcolor || 'white',
-            cursor: url_opts_data.cursor || url_opts_data.fontcolor || 'white'
-          }
-        };
+      join = (ws_url[ws_url.length - 1] === '/' ? '' : '/'),
+      url = ws_url + join + 'ws?id=' + msg.id,
+      sock = new window.WebSocket(url),
+      encoding = 'utf-8',
+      decoder = window.TextDecoder ? new window.TextDecoder(encoding) : encoding,
+      terminal = document.getElementById('terminal'),
+      termOptions = {
+        cursorBlink: true,
+        theme: {
+          background: url_opts_data.bgcolor || 'black',
+          foreground: url_opts_data.fontcolor || 'lightgreen',
+          cursor: url_opts_data.cursor || url_opts_data.fontcolor || 'lightgreen'
+        }
+      };
 
     if (url_opts_data.fontsize) {
       var fontsize = window.parseInt(url_opts_data.fontsize);
@@ -438,13 +438,13 @@ jQuery(function($){
     }
 
 
-    wssh.geometry = function() {
+    wssh.geometry = function () {
       // for console use
       var geometry = current_geometry(term);
       console.log('Current window geometry: ' + JSON.stringify(geometry));
     };
 
-    wssh.send = function(data) {
+    wssh.send = function (data) {
       // for console use
       if (!sock) {
         console.log('Websocket was already closed');
@@ -461,11 +461,11 @@ jQuery(function($){
         sock.send(data);
       } catch (SyntaxError) {
         data = data.trim() + '\r';
-        sock.send(JSON.stringify({'data': data}));
+        sock.send(JSON.stringify({ 'data': data }));
       }
     };
 
-    wssh.reset_encoding = function() {
+    wssh.reset_encoding = function () {
       // for console use
       if (encoding === msg.encoding) {
         console.log('Already reset to ' + msg.encoding);
@@ -474,7 +474,7 @@ jQuery(function($){
       }
     };
 
-    wssh.resize = function(cols, rows) {
+    wssh.resize = function (cols, rows) {
       // for console use
       if (term === undefined) {
         console.log('Terminal was already destroryed');
@@ -483,7 +483,7 @@ jQuery(function($){
 
       var valid_args = false;
 
-      if (cols > 0 && rows > 0)  {
+      if (cols > 0 && rows > 0) {
         var geometry = current_geometry(term);
         if (cols <= geometry.cols && rows <= geometry.rows) {
           valid_args = true;
@@ -497,36 +497,36 @@ jQuery(function($){
       }
     };
 
-    wssh.set_bgcolor = function(color) {
+    wssh.set_bgcolor = function (color) {
       set_backgound_color(term, color);
     };
 
-    wssh.set_fontcolor = function(color) {
+    wssh.set_fontcolor = function (color) {
       set_font_color(term, color);
     };
 
-    wssh.custom_font = function() {
+    wssh.custom_font = function () {
       update_font_family(term);
     };
 
-    wssh.default_font = function() {
+    wssh.default_font = function () {
       reset_font_family(term);
     };
 
-    term.on_resize = function(cols, rows) {
+    term.on_resize = function (cols, rows) {
       if (cols !== this.cols || rows !== this.rows) {
         console.log('Resizing terminal to geometry: ' + format_geometry(cols, rows));
         this.resize(cols, rows);
-        sock.send(JSON.stringify({'resize': [cols, rows]}));
+        sock.send(JSON.stringify({ 'resize': [cols, rows] }));
       }
     };
 
-    term.onData(function(data) {
+    term.onData(function (data) {
       // console.log(data);
-      sock.send(JSON.stringify({'data': data}));
+      sock.send(JSON.stringify({ 'data': data }));
     });
 
-    sock.onopen = function() {
+    sock.onopen = function () {
       term.open(terminal);
       toggle_fullscreen(term);
       update_font_family(term);
@@ -535,20 +535,20 @@ jQuery(function($){
       title_element.text = url_opts_data.title || default_title;
       if (url_opts_data.command) {
         setTimeout(function () {
-          sock.send(JSON.stringify({'data': url_opts_data.command+'\r'}));
+          sock.send(JSON.stringify({ 'data': url_opts_data.command + '\r' }));
         }, 500);
       }
     };
 
-    sock.onmessage = function(msg) {
+    sock.onmessage = function (msg) {
       read_file_as_text(msg.data, term_write, decoder);
     };
 
-    sock.onerror = function(e) {
+    sock.onerror = function (e) {
       console.error(e);
     };
 
-    sock.onclose = function(e) {
+    sock.onclose = function (e) {
       term.dispose();
       term = undefined;
       sock = undefined;
@@ -559,7 +559,7 @@ jQuery(function($){
       title_element.text = default_title;
     };
 
-    $(window).resize(function(){
+    $(window).resize(function () {
       if (term) {
         resize_terminal(term);
       }
@@ -570,11 +570,11 @@ jQuery(function($){
   function wrap_object(opts) {
     var obj = {};
 
-    obj.get = function(attr) {
+    obj.get = function (attr) {
       return opts[attr] || '';
     };
 
-    obj.set = function(attr, val) {
+    obj.set = function (attr, val) {
       opts[attr] = val;
     };
 
@@ -600,21 +600,21 @@ jQuery(function($){
     clean_data(data);
 
     var hostname = data.get('hostname'),
-        port = data.get('port'),
-        username = data.get('username'),
-        pk = data.get('privatekey'),
-        result = {
-          valid: false,
-          data: data,
-          title: ''
-        },
-        errors = [], size;
+      port = data.get('port'),
+      username = data.get('username'),
+      pk = data.get('privatekey'),
+      result = {
+        valid: false,
+        data: data,
+        title: ''
+      },
+      errors = [], size;
 
     if (!hostname) {
       errors.push('Value of hostname is required.');
     } else {
       if (!hostname_tester.test(hostname)) {
-         errors.push('Invalid hostname: ' + hostname);
+        errors.push('Invalid hostname: ' + hostname);
       }
     }
 
@@ -639,7 +639,7 @@ jQuery(function($){
 
     if (!errors.length || debug) {
       result.valid = true;
-      result.title = username + '@' + hostname + ':'  + port;
+      result.title = username + '@' + hostname + ':' + port;
     }
     result.errors = errors;
 
@@ -671,9 +671,9 @@ jQuery(function($){
   function connect_without_options() {
     // use data from the form
     var form = document.querySelector(form_id),
-        inputs = form.querySelectorAll('input[type="file"]'),
-        url = form.action,
-        data, pk;
+      inputs = form.querySelectorAll('input[type="file"]'),
+      url = form.action,
+      data, pk;
 
     disable_file_inputs(inputs);
     data = new FormData(form);
@@ -685,13 +685,13 @@ jQuery(function($){
       button.prop('disabled', true);
 
       $.ajax({
-          url: url,
-          type: 'post',
-          data: data,
-          complete: ajax_complete_callback,
-          cache: false,
-          contentType: false,
-          processData: false
+        url: url,
+        type: 'post',
+        data: data,
+        complete: ajax_complete_callback,
+        cache: false,
+        contentType: false,
+        processData: false
       });
     }
 
@@ -702,9 +702,9 @@ jQuery(function($){
     }
 
     if (pk && pk.size && !debug) {
-      read_file_as_text(pk, function(text) {
+      read_file_as_text(pk, function (text) {
         if (text === undefined) {
-            log_status('Invalid private key: ' + pk.name);
+          log_status('Invalid private key: ' + pk.name);
         } else {
           ajax_post();
         }
@@ -720,8 +720,8 @@ jQuery(function($){
   function connect_with_options(data) {
     // use data from the arguments
     var form = document.querySelector(form_id),
-        url = data.url || form.action,
-        _xsrf = form.querySelector('input[name="_xsrf"]');
+      url = data.url || form.action,
+      _xsrf = form.querySelector('input[name="_xsrf"]');
 
     var result = validate_form_data(wrap_object(data));
     if (!result.valid) {
@@ -739,10 +739,10 @@ jQuery(function($){
     button.prop('disabled', true);
 
     $.ajax({
-        url: url,
-        type: 'post',
-        data: data,
-        complete: ajax_complete_callback
+      url: url,
+      type: 'post',
+      data: data,
+      complete: ajax_complete_callback
     });
 
     return result;
@@ -790,17 +790,16 @@ jQuery(function($){
 
   wssh.connect = connect;
 
-  $(form_id).submit(function(event){
+  $(form_id).submit(function (event) {
     event.preventDefault();
     connect();
   });
 
 
-  function cross_origin_connect(event)
-  {
+  function cross_origin_connect(event) {
     console.log(event.origin);
     var prop = 'connect',
-        args;
+      args;
 
     try {
       args = JSON.parse(event.data);
